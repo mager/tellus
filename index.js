1a6d213baf18befe3e05496614c48b8487ca6eb2
Mager <andrew.mager@gmail.com>
2023-11-23 07:17:45 -0600

    Node 20 and timeout

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+  // Set a timeout of 10 seconds
+  setTimeout(() => {
+    res.status(500).send('Request timed out');
+  }, 10000);
+
+
   res.send('Hello from Node.js backend!');
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(3005, () => {
+  console.log('Server listening on port 3005');
 });
