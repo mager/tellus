96f6938dd6a82dfaff72e72d62b30e1d7131ab10
Mager <andrew.mager@gmail.com>
2023-11-24 16:45:48 -0600

    Better simplify

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const os = require('os');
-const fs = require('fs');
 const axios = require('axios');
-const unzipper = require('unzipper');
 const { exec } = require('child_process');
 const shp = require('shpjs');
-const mapshaper = require('mapshaper');
 const simplify = require('simplify-geojson')
+
 // Setup Express app
 const app = express();
 
@@ -39,10 +36,9 @@ apiRoutes.put('/simplifyGeometry', async (req, res) => {
   try {
     const response = await axios.get(url, { responseType: 'arraybuffer' });
     const data = await shp(response.data);
-    const simplifiedData = simplify(data, 0.01);
     return res.status(200).send({
       "status": "success",
-      "data": simplifiedData
+      "data": simplify(data, 0.01)
     });
   } catch (error) {
     console.error('Error getting shapefile:', error);
@@ -57,62 +53,3 @@ apiRoutes.put('/simplifyGeometry', async (req, res) => {
 app.listen(3005, () => {
   console.log('Server listening on port 3005');
 });
-
-// Function to simplify geometry and generate GeoJSON
-const simplifyGeometry = async (shapefilePath, outputGeoJSONPath) => {
-  const command = `mapshaper -i ${shapefilePath} -o ${outputGeoJSONPath} --dissolve --simplify 50%`;
-  await exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error('Error simplifying geometry:', error);
-    } else {
-      console.log('Geometry simplified successfully!!');
-    }
-  });
-};
-
-const handleZipFile = async (zipFilePath, tempDir) => {
-
-  // const zipStream = response.data;
-  // const tempDir = os.tmpdir() + '/data-' + Date.now();
-  // fs.mkdirSync(tempDir);
-  // console.log(`Created temp dir ${tempDir}`);
-  // let shapeFilename = '';
-
-  // const validShapefileFilenames = ['cpg', 'dbf', 'prj', 'shp', 'shx']
-
-  // zipStream
-  //   .pipe(unzipper.Parse())
-  //   .on('entry', function (entry) {
-  //     const fileName = entry.path;
-  //     console.log(`Unzipping ${fileName}`);
-
-
-  //     const fileExtension = fileName.split('.').pop();
-  //     if (validShapefileFilenames.includes(fileExtension)) {
-  //       console.log(`Found valid shapefile file ${fileName}`);
-  //       entry.pipe(fs.createWriteStream(`${tempDir}/${fileName}`));
-  //       console.log(`Wrote ${fileName} to ${tempDir}`);
-  //       if (fileExtension === 'shp') {
-  //         shapeFilename = fileName;
-  //         console.log({ shapeFilename });
-  //       }
-  //     } else {
-  //       console.log(`Skipping ${fileName}`);
-  //     }
-
-  //     entry.autodrain();
-
-  //   })
-  //   .on('error', (error) => {
-  //     console.error('Error unzipping file:', error);
-  //   })
-  //   .on('finish', () => {
-  //     console.log('Zip file successfully unzipped');
-
-  //     console.log("Let's simplify geometry!")
-  //     const shapefilePath = `${tempDir}/${shapeFilename}`;
-  //     const outputGeoJSONPath = `${tempDir}/output.geojson`;
-  //     simplifyGeometry(shapefilePath, outputGeoJSONPath);
-  //   });
-
-} 
---
+++ util.js
@@ -0,0 +1,57 @@
+
+// Function to simplify geometry and generate GeoJSON
+const mapshaper = async (shapefilePath, outputGeoJSONPath) => {
+    const command = `mapshaper -i ${shapefilePath} -o ${outputGeoJSONPath} --dissolve --simplify 50%`;
+    await exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error('Error simplifying geometry:', error);
+        } else {
+            console.log('Geometry simplified successfully!!');
+        }
+    });
+};
+
+const handleZipFile = async (zipFilePath, tempDir) => {
+    const zipStream = response.data;
+    const tempDir = os.tmpdir() + '/data-' + Date.now();
+    fs.mkdirSync(tempDir);
+    console.log(`Created temp dir ${tempDir}`);
+    let shapeFilename = '';
+
+    const validShapefileFilenames = ['cpg', 'dbf', 'prj', 'shp', 'shx']
+
+    zipStream
+        .pipe(unzipper.Parse())
+        .on('entry', function (entry) {
+            const fileName = entry.path;
+            console.log(`Unzipping ${fileName}`);
+
+
+            const fileExtension = fileName.split('.').pop();
+            if (validShapefileFilenames.includes(fileExtension)) {
+                console.log(`Found valid shapefile file ${fileName}`);
+                entry.pipe(fs.createWriteStream(`${tempDir}/${fileName}`));
+                console.log(`Wrote ${fileName} to ${tempDir}`);
+                if (fileExtension === 'shp') {
+                    shapeFilename = fileName;
+                    console.log({ shapeFilename });
+                }
+            } else {
+                console.log(`Skipping ${fileName}`);
+            }
+
+            entry.autodrain();
+
+        })
+        .on('error', (error) => {
+            console.error('Error unzipping file:', error);
+        })
+        .on('finish', () => {
+            console.log('Zip file successfully unzipped');
+
+            console.log("Let's simplify geometry!")
+            const shapefilePath = `${tempDir}/${shapeFilename}`;
+            const outputGeoJSONPath = `${tempDir}/output.geojson`;
+            mapshaper(shapefilePath, outputGeoJSONPath);
+        });
+} 
