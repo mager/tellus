3b39bdff556cb34792c52517c38596b6ac469c67
Mager <andrew.mager@gmail.com>
2023-11-25 11:24:24 -0600

    Attempt to deploy

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-const express = require('express');
-const simplify = require('./routes/simplify');
+const express = require("express");
+const simplify = require("./routes/simplify");
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/simplify', simplify);
+app.use("/api/simplify", simplify);
 
 app.listen(3005, () => {
-  console.log('Server listening on port 3005');
+  console.log("Server listening on port 3005");
 });
